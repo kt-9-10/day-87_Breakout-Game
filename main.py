0fdae6a542e288ccd025b78d5675c58a190cc69f
ka28-dec <60833391+ka28-dec@users.noreply.github.com>
2024-07-06 14:54:18 +0900

    ver 2.0

--- main.py
+++ main.py
@@ -13,16 +13,19 @@ block_size_x = 3.5
 block_size_y = 1.5
 block_delta_x = block_size_x / 2 * 20 + ball_size / 2 * 20
 block_delta_y = block_size_y / 2 * 20 + ball_size / 2 * 20
+is_bouncing_wall = False
+is_bouncing_block = False
 
 block_pos_list = [
-    (-360, 200), (-280, 200), (-200, 200), (-120, 200), (-40, 200), (40, 200), (120, 200), (200, 200), (280, 200), (360, 200),
-    (-360, 160), (-280, 160), (-200, 160), (-120, 160), (-40, 160), (40, 160), (120, 160), (200, 160), (280, 160), (360, 160),
-    (-360, 120), (-280, 120), (-200, 120), (-120, 120), (-40, 120), (40, 120), (120, 120), (200, 120), (280, 120), (360, 120),
+    (-360, 200), (-280, 200), (-200, 200), (-120, 200), (-40, 200), (40, 200), (120, 200), (200, 200), (280, 200),
+    (360, 200),
+    (-360, 160), (-280, 160), (-200, 160), (-120, 160), (-40, 160), (40, 160), (120, 160), (200, 160), (280, 160),
+    (360, 160),
+    (-360, 120), (-280, 120), (-200, 120), (-120, 120), (-40, 120), (40, 120), (120, 120), (200, 120), (280, 120),
+    (360, 120),
     (-360, 80), (-280, 80), (-200, 80), (-120, 80), (-40, 80), (40, 80), (120, 80), (200, 80), (280, 80), (360, 80),
     (-360, 40), (-280, 40), (-200, 40), (-120, 40), (-40, 40), (40, 40), (120, 40), (200, 40), (280, 40), (360, 40)
 ]
-
-print((paddle_delta_x,paddle_delta_y))
 
 screen = Screen()
 screen.bgcolor("black")
@@ -46,8 +49,7 @@ screen.onkeypress(paddle.go_right, "d")
 screen.onkeypress(paddle.go_left, "Left")
 screen.onkeypress(paddle.go_right, "Right")
 
-is_game_continue = True
-while is_game_continue:
+while True:
     time.sleep(ball.move_speed)
     screen.update()
     ball.move()
@@ -73,9 +75,12 @@ while is_game_continue:
 
     # Detect collision with paddle
     if paddle_col_left < ball_x < paddle_col_right and paddle_col_bottom < ball_y < paddle_col_top:
-        ball.bounce_by_paddle(paddle.towards(ball))
+        if not is_bouncing_wall:
+            ball.bounce_by_paddle(paddle.towards(ball))
+            is_bouncing_wall = True
+    else:
+        is_bouncing_wall = False
 
-    # TODO: Detect collision with Block
     for block in block_list:
         # Get block position
         block_x, block_y = block.pos()
@@ -88,12 +93,27 @@ while is_game_continue:
 
         # Detect collision with block
         if block_col_left < ball_x < block_col_right and block_col_bottom < ball_y < block_col_top:
-            if block_col_left < ball_x < block_col_right:
-                ball.bounce_y()
-            elif block_col_bottom < ball_y < block_col_top:
-                ball.bounce_x()
-            block.hideturtle()
-            block_list.remove(block)
+            if not is_bouncing_block:
+                if block_col_left < ball_x < block_col_right:
+                    ball.bounce_y()
+                elif block_col_bottom < ball_y < block_col_top:
+                    ball.bounce_x()
+                block.hideturtle()
+                block_list.remove(block)
+                is_bouncing_block = True
+        else:
+            is_bouncing_block = False
 
+    # Check Game Clear
+    if not block_list:
+        clear_txt = Turtle()
+        clear_txt.penup()
+        clear_txt.color("gold")
+        clear_txt.hideturtle()
+        clear_txt.clear()
+        clear_txt.goto(0, 0)
+        clear_txt.write("C L E A R", align="center", font=("Courier", 60, "bold"))
+        screen.update()
+        break
 
 screen.exitonclick()
